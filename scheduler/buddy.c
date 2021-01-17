b0ce02094d8083c3d66393dc0db53ea35c793fbb
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-01-17 23:31:25 +0200

    Docs: add documentation to the buddy system module.

--- scheduler/buddy.c
+++ scheduler/buddy.c
@@ -1,3 +1,13 @@
+/**
+ * @file buddy.c
+ * @author Mohamed Hassanin Mohamed
+ * @brief This is a module for the buddy memory mangement system.
+ * @version 0.1
+ * @date 2021-01-10
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +18,14 @@
 int numOfNode = 0;
 node *treeRoot = NULL;
 
-
+/**
+ * @brief Create a node give it's data
+ * 
+ * @param data the size of the memory the node represents.
+ * @param parent a pointer to the parent.
+ * @param start the start of the memory the node represents.
+ * @return node* the created node
+ */
 node *newNode(int data, node *parent, int start)
 {
     node *node1 = (node *)malloc(sizeof(node));
@@ -21,6 +38,20 @@ node *newNode(int data, node *parent, int start)
     return (node1);
 }
 
+/**
+ * @brief This function allocates a piece of memory in the case of
+ * there's no allocated memory in the system or there's no node with a size of
+ * the double desired size in the memory tree.
+ * 
+ * @param size the desired memory size to allocate
+ * @param currentSize the size to start from in the left branch tree
+ *  (i.e. "128", 64, 32 if 32 is the desired and 128 is the currentSize in this case)  
+ * @param grandParent a node to attach the root of the created tree to
+ * @param dir the direction to attach the created tree to 'l' to attach it to the left of
+ * the grand parent and 'r' for the right.
+ * @param start the start of the piece of memory
+ * @return node*a node pointer to the allocated piece of memory
+ */
 node *alloc1(int size, int currentSize, node *grandParent, char dir, int start)
 {
     node *parent;
@@ -51,6 +82,14 @@ node *alloc1(int size, int currentSize, node *grandParent, char dir, int start)
     return t1;
 }
 
+/**
+ * @brief This function is called to allocate a piece of memory in the buddy system
+ * when the tree is not empty (there's some memory allocated in it).
+ * 
+ * @param root the root to the tree
+ * @param size the desired size to allocate
+ * @return node* a node pointer to the allocated piece of memory
+ */
 node *alloc2(node *root, int size)
 {
     if (root == NULL)
@@ -60,8 +99,10 @@ node *alloc2(node *root, int size)
     if (root->left == NULL && root->right == NULL)
         return NULL;
 
+    //if the this node has a size that equals to the double of the desired size
     if (root->data == 2 * size)
     {
+        //If the left is free -> Allocate
         if (root->left == NULL)
         {
             node *t1 = newNode(size, root, root->start);
@@ -70,6 +111,7 @@ node *alloc2(node *root, int size)
 
             return t1;
         }
+        //If the right is free -> Allocate
         else if (root->right == NULL)
         {
             node *t1 = newNode(size, root, root->start + root->data / 2);
@@ -79,10 +121,13 @@ node *alloc2(node *root, int size)
             return t1;
         }
         else
-            return NULL; //there's no point to traverse down
+            //there's no point to traverse down
+            return NULL; 
     }
+    //if the this node has a size that is bigger than the double of the desired size
     else if (root->data > 2 * size)
     {
+        
         if (root->left != NULL && root->right != NULL)
         {
             node *left = alloc2(root->left, size);
@@ -94,6 +139,8 @@ node *alloc2(node *root, int size)
             else
                 return left;
         }
+        //If the tree has left subtree -> solve(left subtree). If it finds a place, 
+        // then the desired memory is allocated. Otherwise it call alloc1 for allocation.
         else if (root->left != NULL)
         {
             node *left = alloc2(root->left, size);
@@ -106,6 +153,8 @@ node *alloc2(node *root, int size)
             else
                 return left;
         }
+        //If the tree has right subtree -> solve(right subtree). If it finds a place, 
+        // then the desired memory is allocated. Otherwise it call alloc1 for allocation.
         else
         {
             node *right = alloc2(root->right, size);
@@ -123,6 +172,13 @@ node *alloc2(node *root, int size)
         return NULL;
 }
 
+/**
+ * @brief This function used to allocate a piece of memory in the buddy system 
+ * memory mangement
+ * 
+ * @param size The desired memory size to allocate
+ * @return node* a node pointer to the allocated piece of memory
+ */
 node *Allocate(int size)
 {
     int blockSize = (int)pow(2, ceil(log(size) / log(2)));
@@ -139,6 +195,13 @@ node *Allocate(int size)
         return alloc2(treeRoot, blockSize);
 }
 
+/**
+ * @brief utility function to deallocate a piece of memory in the buddy system memory 
+ * mangement system.
+ * 
+ * @param root a node to the node to deallocate
+ * @return uint8_t 0 if the tree is empty, 1 otherwise 
+ */
 uint8_t DeallocateUtil(node *root)
 {
     if (root == NULL)
@@ -160,12 +223,24 @@ uint8_t DeallocateUtil(node *root)
 
     return 1;
 }
+
+/**
+ * @brief Deallocate a piece of memory from the buddy system memory mangement system.
+ * 
+ * @param root a node to the memory piece to deallocate.
+ */
 void Deallocate(node *root)
 {
     if (DeallocateUtil(root) == 0)
         treeRoot = NULL;
 }
 
+/**
+ * @brief traverse a binary tree in a preorder way to print its leaves that
+ * represent the memory allocated.
+ * 
+ * @param root the root of the binary tree
+ */
 void PreorderTraverse(node *root)
 {
     if (root == NULL)
@@ -176,3 +251,4 @@ void PreorderTraverse(node *root)
     if (root->left == NULL && root->right == NULL)
         printf("block_size:%d start:%d end:%d\n", root->data, root->start, root->end);
 }
+
--- scheduler/buddy.h
+++ scheduler/buddy.h
@@ -1,16 +1,32 @@
+/**
+ * @file buddy.h
+ * @author Mohamed Hassanin Mohamed
+ * @brief This is a module for the buddy memory mangement system.
+ * @version 0.1
+ * @date 2021-01-10
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #ifndef _BUDDY_H_
 #define _BUDDY_H_
 
 #define MEM_MAX_SIZE 1024
 
+/**
+ * @brief A struct represents a node in a binary tree. It corresponds to
+ * an allocated memory in the simulation if it's a leaf in the tree.
+ * 
+ */
 typedef struct node
 {
-    int data;
-    int start;
-    int end;
-    struct node *left;
-    struct node *parent;
-    struct node *right;
+    int data; 			/**< the size of the node */
+    int start;			/**< the start of the memory piece */
+    int end;			/**< the end of the memory piece */
+    struct node *left;		/**< a node representing the left subtree */
+    struct node *parent;	/**< a node to the parent; equal NULL if there's no parent */
+    struct node *right;		/**< a node representing the right subtree */
 } node;
 
 node *newNode(int data, node *parent, int start);
@@ -18,4 +34,5 @@ node *Allocate(int size);
 void Deallocate(node *root);
 void PreorderTraverse(node *root);
 
-#endif
+#endif /* _BUDDY_H_ */
+
